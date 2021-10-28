9e129ae292e512197730d52bda0d07e614532949
Travis Lang <tlang50@icloud.com>
2021-10-28 02:15:43 -0500

    create structure

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+if (require('electron-squirrel-startup')) {
+    app.quit()
+}
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 500,
+    })
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'))
+
+    mainWindow.webContents.openDevTools({ detach: true })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
---
+++ src/preload.js
@@ -0,0 +1,16 @@
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('myAPI', {
+    desktop: true,
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
